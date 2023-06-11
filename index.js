9c2b2bba4aa9a8686cd5800aec0ba3d1e50b2f49
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-06-11 22:24:48 +0600

    delete select

--- index.js
+++ index.js
@@ -30,16 +30,26 @@ async function run() {
 
 
 
+    const usersCollection = client.db("capturedDb").collection("user");
     const courseCollection = client.db("capturedDb").collection("course");
     const instructorCollection = client.db("capturedDb").collection("instructor");
     const cartsCollection = client.db("capturedDb").collection("cart");
 
 
 
+    //course related apis
+    app.post('/course', async(req,res)=>{
+      const newItem = req.body;
+      const result = await courseCollection.find(newItem).toArray();
+      res.send(result);
+    })
     app.get('/course', async (req,res) => {
         const result = await courseCollection.find().toArray();
         res.send(result)
     })
+
+
+    //instructor related apis
     app.get('/instructor', async (req,res) => {
         const result = await instructorCollection.find().toArray();
         res.send(result)
@@ -50,16 +60,19 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const options = {
         projection: {
+          name:1,
           instructor: 1,
           email: 1,
           instructor_id: 1,
           instructorImg: 1,
           classTaken: 1,
-          postedBy: 1,
-          classes: 1,
+          price: 1,
+          time: 1,
+          image:1,
+          availableSeats:1
         },
       };
-      const result = await instructorCollection.findOne(query, options);
+      const result = await courseCollection.findOne(query, options);
       res.send(result);
     });
 
@@ -71,10 +84,25 @@ async function run() {
       const result = await cartsCollection.insertOne(item);
       res.send(result);
     });
+    
     app.get("/carts", async (req, res) => {
-      const item = req.body;
+      const email = req.query.email;
+      console.log({email});
+      if (!email) {
+        res.send([]);
+        return;
+      }
+      
+      const query = { email: email };
       // console.log(item);
-      const result = await cartsCollection.find(item).toArray();
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
 
