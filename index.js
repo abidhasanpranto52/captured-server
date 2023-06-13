bf2ea1cffa427287dd8e61d729bbf039f1d5a791
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-06-14 00:24:14 +0600

    my course apis

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  // console.log({ message: authorization });
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
 
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkms10x.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,7 +39,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,39 +47,159 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
 
     const usersCollection = client.db("capturedDb").collection("user");
     const courseCollection = client.db("capturedDb").collection("course");
-    const instructorCollection = client.db("capturedDb").collection("instructor");
     const cartsCollection = client.db("capturedDb").collection("cart");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
+    //users related apis
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already Exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        return res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        return res.send({ instructor: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     //course related apis
-    app.post('/course', async(req,res)=>{
+    app.post("/course", verifyJWT, async (req, res) => {
       const newItem = req.body;
-      const result = await courseCollection.find(newItem).toArray();
+      const result = await courseCollection.insertOne(newItem);
       res.send(result);
-    })
-    app.get('/course', async (req,res) => {
-        const result = await courseCollection.find().toArray();
-        res.send(result)
-    })
+    });
+    app.get("/course", async (req, res) => {
+      const result = await courseCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/myCourse/:email", async (req, res) => {
+      const email = req.params.email;
+      const query ={email: email};
+      const result = await courseCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.put("/myCourse/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateCourse = req.body;
+      const updateDoc = {
+        $set: {
+          price: updateCourse.price,
+          seats: updateCourse.seats,
+          description: updateCourse.description,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+
+    app.delete("/myCourse/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
     //instructor related apis
-    app.get('/instructor', async (req,res) => {
-        const result = await instructorCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/instructors", async (req, res) => {
+      const query = { role: "instructor" };
+      const result = await usersCollection.find(query).limit(6).toArray();
+      res.send(result);
+    });
+    
 
     app.get("/instructorInfo/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
         projection: {
-          name:1,
+          name: 1,
           instructor: 1,
           email: 1,
           instructor_id: 1,
@@ -68,33 +207,57 @@ async function run() {
           classTaken: 1,
           price: 1,
           time: 1,
-          image:1,
-          availableSeats:1
+          image: 1,
+          availableSeats: 1,
         },
       };
       const result = await courseCollection.findOne(query, options);
       res.send(result);
     });
 
-    
-//student selected class
-    app.post("/carts", async (req, res) => {
-      const item = req.body;
-      // console.log(item);
-      const result = await cartsCollection.insertOne(item);
-      res.send(result);
-    });
-    
-    app.get("/carts", async (req, res) => {
+
+    app.get("/instructors/:email",verifyJWT, async (req, res) => {
       const email = req.query.email;
-      console.log({email});
       if (!email) {
         res.send([]);
         return;
       }
-      
+
+      const decodedEmail = req.decoded.email;
+      if (email != decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
       const query = { email: email };
-      // console.log(item);
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //student selected class
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.get("/carts",verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+        return;
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email != decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
+      const query = { email: email };
       const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
@@ -108,7 +271,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -116,13 +281,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Captured Moments Server Running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Captured Moments Server Running on Port ${port}`);
-  });
-  
+  res.send("Captured Moments Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Captured Moments Server Running on Port ${port}`);
+});
